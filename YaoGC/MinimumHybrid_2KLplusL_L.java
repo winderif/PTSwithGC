894d162e9a47f2dfee4f94268212e8be3a1a251c
winderif <jack310628@seed.net.tw>
2012-06-08 14:08:44 +0800

    120608 modify MinimumHybrid_2KLplusL_L.java - add ADD circuit

--- YaoGC/MinimumHybrid_2KLplusL_L.java
+++ YaoGC/MinimumHybrid_2KLplusL_L.java
@@ -5,14 +5,17 @@ package YaoGC;
 public class MinimumHybrid_2KLplusL_L extends CompositeCircuit {
     private final int L;
     private final int K;
+    private final int C;
 
     public final static int SUB = 0;
     public final static int MIN = 1;
+    public final static int ADD = 2;
 
     public MinimumHybrid_2KLplusL_L(int l, int k) {
-    	super(2*l*k, l, 2, "Minimum_" + l*k + "_" + l);
+    	super(2*l*k + l, l, 3, "Minimum_" + l*k + "_" + l);
     	L = l;
     	K = k;
+    	C = 2*l*k;
     }
 
     /*
@@ -32,32 +35,40 @@ public class MinimumHybrid_2KLplusL_L extends CompositeCircuit {
     protected void createSubCircuits() throws Exception {
     	subCircuits[SUB] = new SUB_2KL_KL(L, K);
     	subCircuits[MIN] = new Minimum_KL_L(L, K);
+    	subCircuits[ADD] = new ADD_2L_L(L);
     	    	
 		super.createSubCircuits();
     }
 
-    protected void connectWires() throws Exception {    	
+    protected void connectWires() throws Exception {
+    	// connect SUB circuit
     	for(int i=0; i < K; i++) {
     		for(int j=0; j < L; j++) {
     			inputWires[j + L*i + L*K].connectTo(subCircuits[SUB].inputWires, j + L*i + L*K);
     			inputWires[j + L*i      ].connectTo(subCircuits[SUB].inputWires, j + L*i);
     		}
     	}
+    	// connect Minimum circuit
     	for(int i=0; i < K; i++) {
     		for (int j=0; j < L; j++) {
     			subCircuits[SUB].outputWires[j + L*i].connectTo(subCircuits[MIN].inputWires, j + L*i);
     		}
     	}
+    	// connetc ADD circuit
+    	for (int j=0; j < L; j++) {
+    		inputWires[j + C].connectTo(subCircuits[ADD].inputWires, j + L);
+			subCircuits[MIN].outputWires[j].connectTo(subCircuits[ADD].inputWires, j);
+		}
     }
 
     protected void defineOutputWires() {    	
     	for (int i=0; i < L; i++) {
-			outputWires[i] = subCircuits[MIN].outputWires[i];
+			outputWires[i] = subCircuits[ADD].outputWires[i];
 		}    	
     }
 
     private int X(int i) {
-    	return i+L*K;
+    	return i;
     }
 
     private int Y(int i) {
